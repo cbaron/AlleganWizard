b85901a679b0ac78cf47d006eae3890e22ac4dc2
cbaron <topherbaron@gmail.com>
2017-07-07 12:32:25 -0400

    updating app port things

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const fs = require('fs')
 require('node-env-file')( __dirname + '/.env' )
       
 const router = require('./router'),
-    httpsPort = process.env.HTTPS_PORT || 443
+    port =  process.env.HTTP_PORT
 
 router.initialize().then( () => {
     console.log('Router initialized')
-    require('https')
+    require('http')
         .createServer(
-            { key: fs.readFileSync( process.env.SSLKEY ), cert: fs.readFileSync( process.env.SSLCERT ) },
             router.handler.bind(router)
-        ).listen( httpsPort )
-    return Promise.resolve( console.log( `Secure server spinning at port ${httpsPort}` ) )
+        ).listen( port )
+    return Promise.resolve( console.log( `Server spinning at port ${port}` ) )
 } )
 .catch( e => {
     console.log( e.stack || e )
